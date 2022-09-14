368d892be54895c66dd9c1bff6d8af88f52a476e
Antonio David Villegas Yeguas <advy99@gmail.com>
2022-09-14 20:39:27 +0200

    Math: utils module for math operations

---
+++ include/math/utils.hpp
@@ -0,0 +1,26 @@
+#ifndef MATH_UTILS_HPP
+#define MATH_UTILS_HPP
+
+#include <limits>
+#include <type_traits>
+#include <cmath>
+
+namespace cpp_ml::math::utils {
+
+template <typename T> concept is_floating_point = std::is_floating_point_v<T>;
+
+template <typename T>
+requires is_floating_point<T>
+auto are_equal(const T & lhs,
+					const T & rhs,
+					const T epsilon = std::numeric_limits<T>::epsilon()
+) -> bool {
+	return (std::abs(lhs - rhs) < epsilon);
+}
+
+
+
+} // end math::utils namespace
+
+
+#endif
